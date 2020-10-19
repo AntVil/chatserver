06a07f654d518a6e3feb365754edbd3723940c2b
htcryme <56440776+htcryme@users.noreply.github.com>
2020-10-19 14:12:22 +0200

    restructuring

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,31 @@ const express = require('express');
 const path = require("path");
 const app = express();
 const port = 3000;
+const publicFolderPath = path.join(__dirname, "/../", 'public');
+const chatKey = "123";
 
-//app.use(express.static("../public"));
-app.use('/static', express.static(path.join(__dirname, 'public')));
+var users = [];
 
-/*
-app.post("", function(req, res){
+app.use(express.static(publicFolderPath));
 
-});*/
+
+app.post("/login", function (req, res) {
+  console.log("logged in");
+
+  users.push();
+  
+  res.send();
+});
+
+app.post("/submit", function (req, res) {
+  console.log("submitted");
+  res.send();
+});
+
+app.post("/refreshChat", function(){
+  console.log("refreshed chat");
+  res.send();
+})
 
 app.listen(port, () => {
   console.log("serving on port: " + port);
