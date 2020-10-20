78e63d6e6a8bb295021f2b99313a27ccff7f953d
htcryme <56440776+htcryme@users.noreply.github.com>
2020-10-20 14:59:29 +0200

    documenting ChatUser

--- src/public/ChatUser.js
+++ src/public/ChatUser.js
@@ -1,5 +1,7 @@
+//A class for handeling client->server interactions
 class ChatUser {
     constructor(username) {
+        //profile which is sent to the server
         this.userProfile = {
             username: username,
             key: "",
@@ -9,6 +11,7 @@ class ChatUser {
         this.join();
     }
 
+    //method for sending requests
     request(method, url, headerName, headerValue, sendValue) {
         return new Promise(function (resolve, reject) {
             let req = new XMLHttpRequest();
@@ -35,23 +38,27 @@ class ChatUser {
         });
     }
 
+    //method for joining the chatroom
     async join() {
         let response = await this.request("POST", "/join", "Content-Type", "application/json", JSON.stringify(this.userProfile));
         this.userProfile.key = response;
     }
 
+    //method for sending a message (requires complete userProfile)
     async sendMessage(message) {
         this.userProfile.message = message;
         let response = await this.request("POST", "/sendMessage", "Content-Type", "application/json", JSON.stringify(this.userProfile));
         return response;
     }
 
+    //method for receiving messages (requires complete userProfile)
     async getMessages() {
         this.userProfile.message = "";
         let response = await this.request("POST", "/getMessages", "Content-Type", "application/json", JSON.stringify(this.userProfile));
         return response;
     }
 
+    //method for leaving the chatroom (requires complete userProfile)
     async leave() {
         this.userProfile.message = "";
         let response = await this.request("POST", "/leave", "Content-Type", "application/json", JSON.stringify(this.userProfile));
