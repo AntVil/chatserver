5d040697f8b7339bc2a2d059f8c86700b340a87c
htcryme <56440776+htcryme@users.noreply.github.com>
2020-10-20 15:12:01 +0200

    documenting and setting tasks

--- src/public/script.js
+++ src/public/script.js
@@ -1,3 +1,4 @@
+//TODO: send refresh requests every 10sec even when window closed (-> web worker)
 let user;
 
 let frame;
@@ -11,6 +12,7 @@ window.onload = function () {
 
 function loop() {
 
+    //get chat every 120 frames = 2sec
     if (user !== null && frame % 120 === 0) {
         var messages = user.getMessages();
         //TODO: handle messages
@@ -21,9 +23,12 @@ function loop() {
 }
 
 
+
+/* FUNCTIONS CALLED BY USER (BUTTON) */
 function join() {
     let username = document.getElementById("username").value;
     user = new ChatUser(username);
+    //TODO: change appearance
 }
 
 function sendMessage() {
@@ -33,4 +38,5 @@ function sendMessage() {
 
 function leave() {
     user.leave();
+    //TODO: change appearance
 }
