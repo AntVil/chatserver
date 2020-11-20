3298ed24c31c1e32da7c3672b618a927b083db57
Jandorid <73158569+Jandorid@users.noreply.github.com>
2020-11-20 11:40:51 +0100

    update frontend

--- src/public/script.js
+++ src/public/script.js
@@ -6,6 +6,15 @@ let frame;
 window.onload = function () {
     user = null;
 
+    loginScreenElement = document.getElementById("login-screen");
+    chatScreenElement = document.getElementById("chat-screen");
+    usersElement = document.getElementById("users");
+    messagesElement = document.getElementById("messages");
+    chatBoxElement = document.getElementById("chat-box");
+    chatAutoScrollCheckbox = document.getElementById("chat-autoScroll");
+    backgroundElement = document.getElementById("background");
+    aboutElement = document.getElementById("about");
+
     frame = 0;
     loop();
 }
@@ -16,6 +25,7 @@ function loop() {
     if (user !== null && frame % 120 === 0) {
         let messages = user.getMessages();
         //TODO: handle messages
+        renderChat(messages);
         let userList = user.getUsers();
         //TODO: handle users
     }
@@ -23,23 +33,37 @@ function loop() {
     frame++;
     requestAnimationFrame(loop);
 }
+function renderUser(){
 
+}
+function renderChat(chat){
 
+}
+function scrollText() {
+    /* Tried to make autoscroll only if there is no manual scrolling happening, no sucess yet.
+
+    let lastScroll = 0;
+    let down = false;
+    chatBoxElement.on("scroll", function(e) {
+        let scroll = this.scrollHeight;
+        down = scroll > lastScroll;
+        lastScroll = scroll;
+    });
+    */
+    if (chatAutoScrollCheckbox.checked /*&& down*/ ) {
+        chatBoxElement.scrollTop = chatBoxElement.scrollHeight;
+    }
+}
 
 /* FUNCTIONS CALLED BY USER (BUTTON) */
 function join() {
     let username = document.getElementById("username").value;
-<<<<<<< Updated upstream
-    user = new ChatUser(username);
-    //TODO: change appearance
-=======
     user = new ChatUser("ws://localhost:2001/");
 
     loginScreenElement.style.display = "none";
     chatScreenElement.style.display = "flex";
 
     backgroundAnimation();
->>>>>>> Stashed changes
 }
 
 function sendMessage() {
@@ -49,5 +73,6 @@ function sendMessage() {
 
 function leave() {
     user.leave();
-    //TODO: change appearance
+    loginScreenElement.style.display = "flex";
+    chatScreenElement.style.display = "none";
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,60 +1,24 @@
 const express = require('express');
 const path = require("path");
 const fs = require("fs");
-<<<<<<< Updated upstream
-const app = express();
-
-//serve webpage on port
-const port = 3000;
-=======
 const http = require('http');
 const WebSocket = require('ws');
 
->>>>>>> Stashed changes
 
 const serverHeartBeat = 5000;
 const appPort = 2000;
 const serverPort = 2001;
-const publicFolderPath = path.join(__dirname, 'public');
+const publicFolderPath = path.join(__dirname, "/../", 'public');
 const chatFilePath = path.join(__dirname, "chat.txt");
 
 
-<<<<<<< Updated upstream
-
-
-/* CONFIGURATING SERVER */
-app.use(express.json());
-=======
 const app = express();
->>>>>>> Stashed changes
 app.use(express.static(publicFolderPath));
 
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-<<<<<<< Updated upstream
-/* HANDELING REQUESTS */
-app.post("/join", function (req, res) {
-  let userProfile = req.body;
-
-  for(let i=0;i<users.length;i++){
-    if(users[i].username == userProfile.username){
-      res.status(404).send("username taken");
-      return;
-    }
-  }
-
-  let key = "";
-  for (let i = 0; i < 8; i++) {
-    key += String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  }
-  userProfile.key = key;
-  userProfile.timeStamp = Date.now();
-
-  users.push(userProfile);
-=======
->>>>>>> Stashed changes
 
 const USER_NAME = Symbol("username");
 const USER_ID = Symbol("userid");
@@ -62,105 +26,6 @@ const USER_CONNECTED = Symbol("userconnected");
 
 let onlineUsers = [];
 
-<<<<<<< Updated upstream
-app.post("/sendMessage", function (req, res) {
-  let userProfile = req.body;
-  let userIndex = getUserIndex(userProfile);
-  if(userIndex == -1){
-    res.status(404).send("access denied");
-  }else{
-    let message = "#" + users[userIndex].username + "|" + Date.now() + "|" + users[userIndex].message;
-    
-    fs.appendFileSync(chatFilePath, message);
-
-    res.send("message sent");
-  }
-});
-
-
-app.post("/getMessages", function (req, res) {
-  let userProfile = req.body;
-  let userIndex = getUserIndex(userProfile);
-  if(userIndex == -1){
-    res.status(404).send("access denied");
-  }else{
-    res.send(fs.readFileSync(chatFilePath));
-  }
-});
-
-
-app.post("/getUsers", function (req, res) {
-  let userIndex = getUserIndex(userProfile);
-  if(userIndex == -1){
-    res.status(404).send("access denied");
-  }else{
-    let userList = "";
-    for(let i=0;i<users.length;i++){
-      userList += "#" + users[i].username + "|" + users[i].timeStamp;
-    }
-    res.send(userList);
-  }
-});
-
-
-app.post("/leave", function (req, res) {
-  let userProfile = req.body;
-  
-  let userIndex = getUserIndex(userProfile);
-  if(userIndex == -1){
-    res.status(404).send("user not found");
-    return;
-  }else{
-    users.splice(i, 1);
-    res.send("left chat");
-  }
-});
-
-
-app.post("/refresh", function(req, res) {
-  let userIndex = getUserIndex(userProfile);
-  if(userIndex == -1){
-    res.status(404).send("user not found");
-    return;
-  }else{
-    users[userIndex].timeStamp = Date.now();
-    res.send("refreshed");
-  }
-});
-
-
-
-function kick(){
-  //TODO: check 'timeStamp' of users
-  //TODO: unregister user
-  for(let i=users.length-1;i>=0;i--){
-    if(Date.now() - users[i].timeStamp > 10000){
-      users.splice(i, 1);
-    }
-  }
-}
-
-
-
-/* HILFSFUNKTIONEN */
-function getUserIndex(userProfile){
-  let userIndex = -1;
-  for(let i=0;i<users.length;i++){
-    if(users[i].username == userProfile.username && users[i].key == userProfile.key){
-      userIndex = i;
-      break;
-    }
-  }
-  return userIndex;
-}
-
-
-/* START SERVER */
-app.listen(port, () => {
-  console.log("serving on port: " + port);
-  setInterval(kick, 20000);
-});
-=======
 wss.on('connection', function (ws) {
 
   ws[USER_NAME] = "";
@@ -249,4 +114,3 @@ setInterval(function () {
     }
   });
 }, serverHeartBeat);
->>>>>>> Stashed changes
