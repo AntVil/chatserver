f2fae8ee9239532703fe522cd18114642e7eec12
htcryme <56440776+htcryme@users.noreply.github.com>
2020-11-20 11:41:34 +0100

    Update ChatUser.js

--- src/public/ChatUser.js
+++ src/public/ChatUser.js
@@ -1,44 +1,61 @@
 const CHAT_USER_MESSAGES_SYMBOL = Symbol("messages");
 const CHAT_USER_USERS_SYMBOL = Symbol("users");
-const CHAT_USER_CHAT_MESSAGE = 0;
-const CHAT_USER_USERS = 1;
-const CHAT_USER_CHAT_HISTORY = 2;
+const CHAT_USER_USERNAME_SYMBOL = Symbol("username");
+const TYPE_CHAT_USER_MESSAGE = 0;
+const TYPE_CHAT_USER_USERS = 1;
+const TYPE_CHAT_USER_CHAT_HISTORY = 2;
 
-class ChatUser{
-    constructor(url){
+class ChatUser {
+    constructor(username, url) {
         this.webSocket = new WebSocket(url);
+        this.webSocket[CHAT_USER_USERNAME_SYMBOL] = username;
         this.webSocket[CHAT_USER_MESSAGES_SYMBOL] = [];
 
-        this.webSocket.onmessage = function(event){
+        this.webSocket.onopen = function () {
+            this.send(JSON.stringify({
+                "type": 0,
+                "data": this[CHAT_USER_USERNAME_SYMBOL]
+            }));
+        }
+
+        this.webSocket.onmessage = function (event) {
             let message = JSON.parse(event.data);
-            if(message.type === TYPE_CHAT_MESSAGE){
+
+            if (message.type === TYPE_CHAT_USER_MESSAGE) {
                 this[CHAT_USER_MESSAGES_SYMBOL].push(message);
-            }else if(message.type === TYPE_CHAT_USER){
-                this[CHAT_USER_USERS_SYMBOL].push(message);
-            }else if(message.type === CHAT_USER_CHAT_HISTORY){
-                for(let i=0;i<messages.length;i++){
-                    this[CHAT_USER_MESSAGES_SYMBOL].push(message);
-                }
+
+            } else if (message.type === TYPE_CHAT_USER_USERS) {
+                this[CHAT_USER_USERS_SYMBOL] = message.split("|");
+
+            } else if (message.type === TYPE_CHAT_USER_CHAT_HISTORY) {
+                this[CHAT_USER_MESSAGES_SYMBOL] = message.split("|");
             }
         }
     }
 
-    getNewMessages(){
+    getUsers() {
+        return this.webSocket[CHAT_USER_USERS];
+    }
+
+    getNewMessages() {
         let messages = this.webSocket[CHAT_USER_MESSAGES_SYMBOL];
         this.webSocket[CHAT_USER_MESSAGES_SYMBOL] = [];
         return messages;
     }
 
-    sendMessage(message){
-        if(this.webSocket.readyState === WebSocket.OPEN){
-            this.webSocket.send(message);
+    sendMessage(message) {
+        if (this.webSocket.readyState === WebSocket.OPEN) {
+            this.webSocket.send(JSON.stringify({
+                "type": 0,
+                "data": message
+            }));
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    leave(){
+    leave() {
         this.webSocket.close();
     }
 }
@@ -47,11 +64,11 @@ class ChatUser{
 got messages
 
 {
-    chatHistory: [],
-    id: String,
-    type: int,
-    data: "123213123<br>123",
-    time: int
+    id: String,        //user id when logging in first time
+    type: int,         //type of message
+    data: String | [],      //data send; message | chathistory
+    time: int,         //time of sending
+    user: String    //by who
 }
 
 send messages
